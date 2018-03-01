02a26363a6493a91c76919396ccace107fe97f05
Lukas Walker <lukas.walker@bluewin.ch>
2018-03-01 18:32:11 +0100

    Main initial push

---
+++ HashCode2018/src/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // Create a new Scanner object for reading the input
+        Scanner sc = new Scanner(System.in);
+
+        // Read the number of testcases to follow
+        int t = sc.nextInt();
+
+        // Iterate over the testcases and solve the problem
+        for (int i = 0; i < t; i++) {
+
+        }
+    }
+
+
+}
+
