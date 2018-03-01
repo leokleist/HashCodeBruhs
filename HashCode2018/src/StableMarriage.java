4a1b2fe007b2d71cd3ed34189c52bab36198c756
Lukas Walker <lukas.walker@bluewin.ch>
2018-03-01 21:08:22 +0100

    stable marriage

---
+++ HashCode2018/src/StableMarriage.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StableMarriage {
+
+    List<Car> cars;
+    List<Ride> rides;
+
+    List<Car> allCars = new ArrayList<>();
+
+    Map<Ride, Car> marriage = new HashMap<>();
+
+    public static void main(String[] args)
+    {
+
+    }
+
+
+    StableMarriage(List<Car> cars, List<Ride> rides)
+    {
+        for(Car car : cars)
+        {
+            allCars.add(car);
+        }
+
+
+        while(cars.size() > 0)
+        {
+            List<Car> carsWithRide = new ArrayList<>();
+
+            while(marriage.size() < cars.size())
+            {
+                for(Car car : cars)
+                {
+                    if(carsWithRide.contains(car)) continue;
+
+                    Ride ride = car.chooseBest(rides);
+
+                    //find ride
+                    if(marriage.containsKey(ride))
+                    {
+                        if(ride.getQuality(marriage.get(ride)) < ride.getQuality(car))
+                        {
+                            marriage.remove(ride);
+                            carsWithRide.remove(car);
+
+
+                            marriage.put(ride, car);
+                            carsWithRide.add(car);
+                        }
+
+                    }
+
+                    //exclude car if no rides for car
+                    if(car.excluded.size() >= rides.size()) cars.remove(car);
+                }
+            }
+
+            for(Ride ride : marriage.keySet())
+            {
+                marriage.get(ride).addRide(ride);
+                rides.remove(ride);
+            }
+        }
+    }
+
+
+}
