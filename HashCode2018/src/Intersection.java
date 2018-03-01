01504b2c448443bdaa0dba431832d21761ffe3dc
andregraubner <andregr@student.ethz.ch>
2018-03-01 20:12:38 +0100

    Intersection distance

--- HashCode2018/src/Intersection.java
+++ HashCode2018/src/Intersection.java
@@ -8,4 +8,9 @@ public class Intersection {
         this.x = x;
         this.y = y;
     }
+
+    // Get distance of two intersections
+    public static int distance(Intersection i1, Intersection i2) {
+        return Math.abs(i1.x - i2.x) + Math.abs(i1.y - i2.y);
+    }
 }
