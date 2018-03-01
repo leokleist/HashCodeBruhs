d11cf3b1157275352264806bfa9d8d7e47a941de
andregraubner <andregr@student.ethz.ch>
2018-03-01 20:01:21 +0100

    Reading input; Creating List of Cars and Rides

--- HashCode2018/src/Main.java
+++ HashCode2018/src/Main.java
@@ -7,18 +7,42 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+
+        List<Car> cars = new List<Car>();
+        List<Ride> rides = new List<Ride>();
+
         // Create a new Scanner object for reading the input
         Scanner sc = new Scanner(System.in);
 
-        // Read the number of testcases to follow
-        int t = sc.nextInt();
-        
-        // Iterate over the testcases and solve the problem
-        for (int i = 0; i < t; i++) {
+        // Read header
+        int R = sc.nextInt();
+        int C = sc.nextInt();
+        int F = sc.nextInt();
+        int N = sc.nextInt();
+        int B = sc.nextInt();
+        int T = sc.nextInt();
 
+        // Create cars
+        for(int i = 0; i < N; i++) {
+            cars.add(new Car(new Intersection(0, 0)));
+        }
+
+        // Read rides
+        for(int i = 0; i <= N; i++) {
+            Scanner line = new Scanner(sc.nextLine());
+
+            int a = line.nextInt();
+            int b = line.nextInt();
+            int x = line.nextInt();
+            int y = line.nextInt();
+            int s = line.nextInt();
+            int f = line.nextInt();
+
+            Intersection start = new Intersection(a, b);
+            Intersection end = new Intersection(x, y);
+
+            rides.add(new Ride(start, end, s, f));
         }
     }
-
-
 }
 
