414d407a92e005eb999960a1b43a10f60f735d6b
Lukas Walker <lukas.walker@bluewin.ch>
2018-03-01 20:24:36 +0100

    changed car

--- HashCode2018/src/Car.java
+++ HashCode2018/src/Car.java
@@ -1,10 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Car {
 
     Intersection intersection;
+
+    List<Ride> excluded = new ArrayList<>();
+
+    int currentStep = 0;
 
     public Car(Intersection intersection)
     {
         this.intersection = intersection;
     }
 
+    public Ride chooseBest(List<Ride> rides)
+    {
+        Ride maxRide = rides.get(0);
+        double maxPoints = 0;
+
+        for (Ride ride : rides)
+        {
+            double points = ride.getQuality(this);
+
+            if(points > maxPoints)
+            {
+                maxPoints = points;
+                maxRide   = ride;
+            }
+        }
+
+        excluded.add(maxRide);
+
+        return maxRide;
+    }
 }
